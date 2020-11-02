e7e3bff26d5d9f2d842403f7d37b1f9aadc2f1a5
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 15:58:14 +0000

    Add files via upload

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For is a more controlled alternative to the while loop
+            for (int j = 0; j < 5; j++) {
+                for(int k = 0; k < 3; k++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+            //foreach allows us to iterate over a collection
+            int[] ages = {19, 20, 30, 40 , 50};
+
+            foreach(int age in ages)
+            {
+                Console.WriteLine(age);
+            }
+        }
+    }
+}
